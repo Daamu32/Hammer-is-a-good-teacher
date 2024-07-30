ee2375ca425b691a4efe29aa30bce61b73b4b46f
Daamu32 <dgdgdrdr@gmail.com>
2024-07-30 12:11:55 +0200

    "A small visual change in RemoveLogs.log and the exclusion in gitignore of the .vscode folder."

--- obj/Debug/net8.0/PruebaDepuraciónDiscord.AssemblyInfo.cs
+++ obj/Debug/net8.0/PruebaDepuraciónDiscord.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PruebaDepuraciónDiscord")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+368b55a8ecae487096d0cf4f7ed452b08e9b8249")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71986b105524961a75ac6f3f86905652cc918f59")]
 [assembly: System.Reflection.AssemblyProductAttribute("PruebaDepuraciónDiscord")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PruebaDepuraciónDiscord")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
